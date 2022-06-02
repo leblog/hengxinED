01abbe1ad54d0b940e3c6bdb79207777c829fb30
leblog <hanlele96@163.com>
2022-06-02 18:49:56 +0800

    docs: 数据更新

---
+++ sql/hengxin/hx_taste_detail.sql
@@ -0,0 +1,53 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 本机
+ Source Server Type    : MySQL
+ Source Server Version : 50719
+ Source Host           : localhost:3306
+ Source Schema         : vue
+
+ Target Server Type    : MySQL
+ Target Server Version : 50719
+ File Encoding         : 65001
+
+ Date: 31/05/2022 15:37:09
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for hx_taste_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `hx_taste_detail`;
+CREATE TABLE `hx_taste_detail`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '口味明细ID',
+  `taste_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '口味名称',
+  `taste_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '管理口味表ID',
+  `taste_detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '口味描述',
+  `is_basic_taste` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '有基础口味（0:是，1：否）',
+  `basic_taste_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基础口味名称/编号',
+  `basic_taste_improvement_ideas` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '基础口味改善建议',
+  `capacity` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '容量',
+  `vg` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'VG（待优化）',
+  `nic_type` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'NIC类别（字段长度待优化)',
+  `nic_concentration` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'NIC浓度',
+  `nic_unit` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT 'NIC单位（mg/ml ， mg/g）',
+  `perfumer` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '调香师',
+  `version` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '版本',
+  `create_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '创建者',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '更新者',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '更新时间',
+  `remark` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT '' COMMENT '备注',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 29 CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '口味申请单明细表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of hx_taste_detail
+-- ----------------------------
+INSERT INTO `hx_taste_detail` VALUES (27, '测试1', '1531196444072964097', '测试1', '0', '测试1', '测试1', '测试1', '测试1', '测试1', '测试1', '0', '测试1', '测试内容', NULL, NULL, NULL, NULL, '');
+INSERT INTO `hx_taste_detail` VALUES (28, '测试2', '1531196444072964097', '测试1', '1', '测试2', '测试2', '测试2', '测试2', '测试2', '测试2', '0', '测试2', '测试212232341234', NULL, NULL, NULL, NULL, '');
+
+SET FOREIGN_KEY_CHECKS = 1;
